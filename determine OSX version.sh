76149a11be6399ae7d8a23f3ed2609a4faf66c8e
jreitzersmith <jreitzersmith+github@gmail.com>
2015-11-09 17:42:40 -0600

    sync

---
+++ determine OSX version.sh
@@ -0,0 +1,16 @@
+##determine OS version
+		OSVer=$( sw_vers -productVersion )
+		CasperLog "Current OS: $OSVer"
+
+		## Set appropriate installer flag based on OS version.
+		## Handles packages that may have an old cert
+		## TODO: rewrite as a case statement for the 3 nanoseconds performance improvement
+		if [[ $OSVer =~ "10.6" ]]; then
+			allowFlag="-allow"
+		elif [[ $OSVer =~ "10.7" ]]; then
+			allowFlag="-allowUntrusted"
+		elif [[ $OSVer =~ "10.8" ]]; then
+			allowFlag="-allowUntrusted"
+		elif [[ $OSVer =~ "10.10" ]]; then
+			allowFlag="-allowUntrusted"    
+		fi
